d176bbbf48ffed53ee526ab375a503a358ab0bee
Deeksha-Jagadeeshwaran <deekshajagadeeshwaran@gmail.com>
2024-07-03 22:00:13 +0530

    first commit

---
+++ JavaScript_Programming_Exercises-master/02_JS_variables/Ch2_pgm_03.js
@@ -0,0 +1,28 @@
+// Using a variable
+
+var score;
+score = 100;
+console.log(score);
+
+var playername;
+playername = "dia";
+console.log(playername);
+
+var playerage;
+playerage= 18;
+console.log(playerage);
+
+
+
+/* Further Adventures
+ *
+ * 1) Change the value assigned to score.
+ *    Run the program again.
+ *
+ * 2) Declare a second variable, maybe score2.
+ *
+ * 3) Assign your variable a value.
+ *
+ * 4) Use console.log to display your variable on the console.
+ *
+ */
---
+++ JavaScript_Programming_Exercises-master/03_JS_Objects/Ch3_pgm_01.js
@@ -0,0 +1,39 @@
+// Using variables to represent a book
+
+var bookTitle;
+var bookAuthor;
+
+bookTitle = "The Hobbit";
+bookAuthor = "J. R. R. Tolkien";
+
+console.log(bookTitle + " by " + bookAuthor);
+
+var book2Title;
+var book2Author;
+
+book2Title = "Northern Lights";
+book2Author = "Philip Pullman";
+
+console.log(book2Title + " by " + book2Author);
+
+var book3Title;
+var book3Author;
+
+book3Title = "The Adventures of Tom Sawyer";
+book3Author = "Mark Twain";
+
+console.log(book3Title + " by " + book3Author);
+
+/* Further Adventures
+ *
+ * 1) Declare variables for a second book
+ *    and assign them values.
+ *
+ * 2) Add code to log its details to the console.
+ *
+ * 3) Repeat steps 1 and 2 for a third book.
+ *
+ * 4) Consider the code needed for 10 books.
+ *    For 100 books.
+ *
+ */
